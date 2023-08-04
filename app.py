0826683c965d5d877511790e6dc40391f5a43c4d
manas95826 <74074241+manas95826@users.noreply.github.com>
2023-08-04 20:09:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,29 +10,36 @@ try:
 
     with open("email_vectorizer.pkl", "rb") as f:
         vectorizer = pickle.load(f)
-        
-st.title("Email Spam Classification")
-st.write("Enter the email text below and click 'Classify' to determine if it's spam or not.")
 
-# Get user input text
-input_text = st.text_input("Enter email text")
+    st.title("Email Spam Classification")
+    st.write("Enter the email text below and click 'Classify' to determine if it's spam or not.")
 
-def main():
-    if input_text:
-        # Preprocess input text
-        preprocessed_text = preprocess_text(input_text)
-        
-        # Create a vector using the loaded vectorizer's vocabulary
-        input_vector = [preprocessed_text]
-        input_array = vectorizer.fit_transform(input_vector).toarray()
-        
-        # Make prediction
-        prediction = model.predict(input_array)[0]
+    # Get user input text
+    input_text = st.text_input("Enter email text")
 
-        if prediction:
-            st.error("Spam")
-        else:
-            st.success("Not Spam")
+    def preprocess_text(text):
+        # Implement your text preprocessing logic here
+        # For example, you can convert text to lowercase and remove special characters
+        text = text.lower()
+        text = re.sub(r'[^a-zA-Z\s]', '', text)
+        return text
 
-if __name__ == "__main__":
-    main()
+    def main():
+        if input_text:
+            # Preprocess input text
+            preprocessed_text = preprocess_text(input_text)
+
+            # Create a vector using the loaded vectorizer's vocabulary
+            input_vector = [preprocessed_text]
+            input_array = vectorizer.transform(input_vector).toarray()
+
+            # Make prediction
+            prediction = model.predict(input_array)[0]
+
+            if prediction:
+                st.error("Spam")
+            else:
+                st.success("Not Spam")
+
+    if __name__ == "__main__":
+        main()
