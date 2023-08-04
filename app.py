88aed62fb35b7de74f9624faaef364f1ad42a0b4
manas95826 <74074241+manas95826@users.noreply.github.com>
2023-08-04 18:32:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
+import re
 
 # Load model and vectorizer
 try:
@@ -18,10 +19,19 @@ st.write("Enter the email text below and click 'Classify' to determine if it's s
 # Get user input text
 input_text = st.text_input("Enter email text")
 
+def preprocess_text(text):
+    # Apply preprocessing steps to input text
+    text = text.lower()  # Convert to lowercase
+    text = re.sub(r'\W', ' ', text)  # Remove non-word characters
+    return text
+
 def main():
     if input_text:
-        # Preprocess input text using the loaded vectorizer
-        input_vector = vectorizer.transform([input_text])
+        # Preprocess input text using the same steps as during training
+        preprocessed_text = preprocess_text(input_text)
+        
+        # Transform preprocessed text using the loaded vectorizer
+        input_vector = vectorizer.transform([preprocessed_text])
         
         # Convert input_vector to a dense array
         input_array = input_vector.toarray()
