9f36b9f67a85d19d7ac2ad19c7eb72f8d3eaf018
manas95826 <74074241+manas95826@users.noreply.github.com>
2023-08-04 21:18:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 import re
-import numpy as np
 
 # Load model and vectorizer
 try:
     with open("email_spam_pipeline.pkl", "rb") as f:
         model = pickle.load(f)
 
-    # with open("email_vectorizer.pkl", "rb") as f:
-    #     vectorizer = pickle.load(f)
+    with open("email_vectorizer.pkl", "rb") as f:
+        vectorizer = pickle.load(f)
 
     st.title("Email Spam Classification")
     st.write("Enter the email text below and click 'Classify' to determine if it's spam or not.")
@@ -18,23 +17,22 @@ try:
     # Get user input text
     input_text = st.text_input("Enter email text")
 
-    # def preprocess_text(text):
-    #     # Implement your text preprocessing logic here
-    #     # For example, you can convert text to lowercase and remove special characters
-    #     text = text.lower()
-    #     text = re.sub(r'[^a-zA-Z\s]', '', text)
-    #     return text
+    def preprocess_text(text):
+        text = text.lower()
+        text = re.sub(r'[^a-zA-Z\s]', '', text)
+        return text
 
     def main():
         if input_text:
-            # Convert input text to a list of character ordinals
-            array_representation = [ord(char) for char in input_text]
+            # Preprocess input text
+            preprocessed_text = preprocess_text(input_text)
 
-            # Convert the list to a numpy array
-            array_representation = np.array(array_representation)
+            # Use the loaded vectorizer to transform the preprocessed text
+            input_vector = vectorizer.transform([preprocessed_text])
+            input_array = input_vector.toarray()
 
             # Make prediction
-            prediction = model.predict(array_representation.reshape(1, -1))[0]
+            prediction = model.predict(input_array)[0]
 
             if prediction:
                 st.error("Spam")
